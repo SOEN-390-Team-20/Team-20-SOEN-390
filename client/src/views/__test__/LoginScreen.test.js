b9a3bbedaf64466923ef28091cf2a9946aa55e00
zaad97 <saad_ahmed1997@hotmail.com>
2022-02-15 22:39:50 -0500

    Update Lint

--- client/src/views/__test__/LoginScreen.test.js
+++ client/src/views/__test__/LoginScreen.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
 import { toBeInTheDocument } from '@testing-library/jest-dom';
 import LoginScreen from '../LoginScreen';
 
