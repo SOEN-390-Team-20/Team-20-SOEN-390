79174b998eb02bb1afbea2c6c28dfcc76b972bab
noorhammodi <noorhammodi@gmail.com>
2022-04-19 17:44:23 -0400

    added button an dpop up

--- client/src/views/Appointments.js
+++ client/src/views/Appointments.js
@@ -12,6 +12,12 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import doctorPatients from '../services/doctorPatients';
 import Sidebar from '../components/Sidebar';
 
@@ -45,7 +51,15 @@ function Appointments() {
     }
     fetchMyAPI();
   }, []);
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <Sidebar />
@@ -76,7 +90,7 @@ function Appointments() {
             <TableHead>
               <TableRow>
                 <StyledTableCell>Name</StyledTableCell>
-                <StyledTableCell align="right">last appointement</StyledTableCell>
+                <StyledTableCell align="right">Last appointement</StyledTableCell>
                 <StyledTableCell align="right">Location</StyledTableCell>
                 <StyledTableCell align="right">Status</StyledTableCell>
               </TableRow>
@@ -93,8 +107,30 @@ function Appointments() {
                   <StyledTableCell align="right">
                     {row.action}
                     <Stack spacing={2} direction="row-reverse">
-                      <Button variant="outlined">Previous</Button>
-                      <Button variant="outlined">Next</Button>
+                      <Button variant="outlined" onClick={handleClickOpen}>Book next Appointment</Button>
+                      <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                      >
+                        <DialogTitle id="alert-dialog-title">
+                          Request sent
+                          {' '}
+                          <CheckBoxIcon fontsize="80" color="success" />
+                        </DialogTitle>
+                        <DialogContent>
+                          <DialogContentText id="alert-dialog-description">
+                            We have sent a request to the patient.
+                            When patient confirms appointment date, you will be notified.
+                          </DialogContentText>
+                        </DialogContent>
+
+                        <DialogActions>
+                          <Button onClick={handleClose}>Close</Button>
+
+                        </DialogActions>
+                      </Dialog>
                     </Stack>
 
                   </StyledTableCell>
